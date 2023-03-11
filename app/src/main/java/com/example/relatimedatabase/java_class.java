817a71c1156fcf5c326f69d6d95661ea61a16fb8
afrintuli <tuli1907019@stud.kuet.ac.bd>
2023-03-11 23:11:16 +0600

    Ihave created a new java_class

---
+++ app/src/main/java/com/example/relatimedatabase/java_class.java
@@ -0,0 +1,26 @@
+package com.example.relatimedatabase;
+
+public class java_class {
+    String name,id;
+
+    public java_class(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
