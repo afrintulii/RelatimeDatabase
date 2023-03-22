b84f780cb6c48ee5278326a6fed5fab27e07dd02
afrintuli <tuli1907019@stud.kuet.ac.bd>
2023-03-22 21:15:50 +0600

    I  teach mahi

---
+++ app/src/main/java/com/example/relatimedatabase/java_class3.java
@@ -0,0 +1,26 @@
+package com.example.relatimedatabase;
+
+public class java_class3 {
+    int roll,age;
+
+    public java_class3(int roll, int age) {
+        this.roll = roll;
+        this.age = age;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
